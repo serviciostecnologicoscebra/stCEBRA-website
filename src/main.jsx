fb4dd85da2a8b506933b0a145111eae25de55d71
cebra <serviciostecnologicoscebra@gmail.com>
2026-05-30 18:24:49 -0500

    import toaster

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
+import { Toaster } from 'react-hot-toast'
 import './styles/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Toaster />
     <App />
   </React.StrictMode>,
 )
