e81eff312e6b62419d3f32e82640a7dd98a59721
cebra <serviciostecnologicoscebra@gmail.com>
2026-06-18 11:57:22 -0500

    fix img btn

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,51 +1,93 @@
+import { useState } from 'react'
 import './Services.scss'
 import imgServices from '../../assets/images/services.png'
-
+import imgAccesorios from '../../assets/images/accesorios.png'
+import imgRepuestos from '../../assets/images/repuestos.png'
+import imgRepotenciacion from '../../assets/images/repotenciacion.png'
+import imgSolucionesTi from '../../assets/images/soluciones-ti.png'
+import imgInstalacionProgramas from '../../assets/images/instalacion-programas.png'
+import imgInstalacionRed from '../../assets/images/instalacion-red.png'
+import imgInstalacionCamaras from '../../assets/images/instalacion-camaras.png'
+import imgDesarrolloWeb from '../../assets/images/desarrollo-web.png'
 const services = [
   {
-    icon: '🎧',
+    icon: <img src={imgAccesorios}
+               alt="imgAccesorios"
+               className="img-services"/>,
     title: 'Accesorios de Tecnología',
     desc: 'Los mejores accesorios para mejorar tu experiencia.',
+    whatsapp:
+    'Hola, me interesa el servicio de Accesorios de Tecnología.'
   },
   {
-    icon: '💾',
+    icon: <img src={imgRepuestos}
+               alt="imgRepuestos"
+               className="img-services"/>,
     title: 'Repuestos Informáticos',
     desc: 'Componentes originales y compatibles con garantía.',
+    whatsapp:
+      'Hola, me interesa el servicio de Repuestos Informáticos.'
   },
   {
-    icon: '💻',
-    title: 'Repotenciación de PCs',
+    icon: <img src={imgRepotenciacion}
+               alt="imgRepotenciacion"
+               className="img-services"/>,
+    title: 'Repotenciación de PC y Laptop',
     desc: 'Mejoramos el rendimiento de tus equipos.',
+    whatsapp:
+      'Hola, me interesa el servicio de Repotenciación'
   },
   {
-    icon: '🛠️',
+    icon: <img src={imgSolucionesTi}
+               alt="imgSolucionesTi"
+               className="img-services"/>,
     title: 'Problemas de TI e Informática',
     desc: 'Solucionamos errores de hardware y software.',
+     whatsapp:
+      'Hola, tengo un problema de TI.'
   },
   {
-    icon: '🪟',
+    icon: <img src={imgInstalacionProgramas}
+               alt="imgInstalacionProgramas"
+               className="img-services"/>,
     title: 'Instalación y Activación de Programas Originales',
     desc: 'Software original y configuraciones profesionales.',
+     whatsapp:
+      'Hola, me interesa el servicio de Instalación de Programas Originales.'
   },
   {
-    icon: '🌐',
+    icon: <img src={imgDesarrolloWeb}
+               alt="imgDesarrolloWeb"
+               className="img-services"/>,
     title: 'Desarrollo de Páginas y Sistemas Web',
     desc: 'Creamos páginas y sistemas modernos.',
+     whatsapp:
+      'Hola, me interesa el servicio de Desarrollo de Paginas Web.'
   },
   {
-    icon: '📹',
+    icon: <img src={imgInstalacionCamaras}
+               alt="imgInstalacionCamaras"
+               className="img-services"/>,
     title: 'Cámaras de Seguridad',
     desc: 'Videovigilancia profesional para hogares y negocios.',
+     whatsapp:
+      'Hola, me interesa el servicio de Cámaras de Seguridad.'
   },
   {
-    icon: '🔌',
+    icon: <img src={imgInstalacionRed}
+               alt="imgInstalacionRed"
+               className="img-services"/>,
     title: 'Instalación de Cableado Estructurado de Red',
     desc: 'Redes estables, organizadas y de alto rendimiento.',
+     whatsapp:
+      'Hola, me interesa el servicio de Cableado Estructurado de Red.'
   },
 ]
 
 function Services() {
+  const [selectedService, setSelectedService] = useState(null)
   return (
+    <>
     <section className="services" id="servicios">
 
       <div className="container">
@@ -66,7 +108,7 @@ function Services() {
           <div>
           <img
                     src={imgServices}
-                    alt="CEBRA"
+                    alt="imgServices"
                     className="floating-services"
                   />
                   </div>
@@ -75,10 +117,20 @@ function Services() {
         <div className="services-grid">
 
           {services.map((service, index) => (
-            <div className="service-card" key={index}>
+            <div
+              className="service-card"
+              key={index}
+
+              onClick={() =>
+
+                setSelectedService(service)
+
+              }
+
+            >
              <div className="service-left">
               <div className="service-image">
-                <span>{service.icon}</span>
+                {service.icon}
               </div>
 
               <div className="service-content">
@@ -95,6 +147,80 @@ function Services() {
       </div>
 
     </section>
+    {
+    selectedService && (
+
+    <div className="modal-overlay">
+
+      <div className="modal">
+
+        <button
+
+          className="close-btn"
+
+          onClick={() =>
+
+            setSelectedService(null)
+
+          }
+
+        >
+
+          ✕
+
+        </button>
+
+        <div className="modal-image">
+
+          {selectedService.icon}
+
+        </div>
+
+        <h2>
+
+          {selectedService.title}
+
+        </h2>
+
+        <p>
+
+          {selectedService.desc}
+
+        </p>
+
+        <button
+
+          className="whatsapp-btn"
+
+          onClick={() =>
+
+            window.open(
+
+              `https://wa.me/51906257485?text=${encodeURIComponent(
+
+                selectedService.whatsapp
+
+              )}`,
+
+              '_blank'
+
+            )
+
+          }
+
+        >
+
+          Solicitar por WhatsApp
+
+        </button>
+
+      </div>
+
+    </div>
+
+    )
+    }
+    </>
   )
 }
 
