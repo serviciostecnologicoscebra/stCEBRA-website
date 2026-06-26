fdfdda464db02bc8f293f3ec98ef011cadacd452
cebra <serviciostecnologicoscebra@gmail.com>
2026-06-25 20:19:43 -0500

    img hero mundial

--- src/modules/hero/heroImages.js
+++ src/modules/hero/heroImages.js
@@ -1,9 +1,13 @@
 import logo from '../../assets/images/logo.png'
-
+import mascotasMundial from '../../assets/images/mascotas-mundial.png'
 export const heroImages = [
   {
     src: logo,
     className: ''
   },
+  {
+    src: mascotasMundial,
+    className: 'mascotas-logo'
+  }
   
 ]
